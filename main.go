add7fbd50d7d8f0dd9b0316c994c49f5a1cb243a
Vincent Ma <siyan.ma@outlook.com>
2016-09-08 02:21:32 -0400

    added files

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/sessions"
+)
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+
+func main() {
+	router := gin.Default()
+
+	store, _ := sessions.NewRedisStore(10, "tcp", "localhost:6379", "", []byte("secret"))
+	router.Use(sessions.Sessions("session", store))
+
+	router.Use(Cors())
+
+	// router setup (dummy data)
+	router.GET("/incr", func(c *gin.Context) {
+		session := sessions.Default(c)
+		var count int
+		v := session.Get("count")
+		if v == nil {
+			count = 0
+		} else {
+			count = v.(int)
+			count += 1
+		}
+		session.Set("count", count)
+		session.Save()
+		c.JSON(200, gin.H{"count": count})
+	})
+
+	router.Run(":8080")
+}
